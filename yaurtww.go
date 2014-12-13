543df4c94412ec5b812e1b580189d84d15717ef7
Jon Chen <bsd@voltaire.sh>
2014-12-12 22:26:45 -0500

    RequiredFlag()

--- yaurtww.go
+++ yaurtww.go
@@ -1,6 +1,10 @@
 package yaurtww
 
-import flag "github.com/docker/docker/pkg/mflag"
+import (
+	"log"
+
+	flag "github.com/docker/docker/pkg/mflag"
+)
 
 type Manifest struct {
 	Version string
@@ -12,9 +16,20 @@ type ManifestAsset struct {
 	FileName string
 }
 
-const CDN_URL = "http://cdn.urbanterror.info/urt/%s/%s/q3ut4/%s"
+// const CDN_URL = "http://cdn.urbanterror.info/urt/%s/%s/q3ut4/%s"
+const CDN_URL = "http://cdn.urbanterror.info/urt/"
 
 var (
 	ManifestPath *string
 	DownloadPath = flag.String([]string{"d", "-dest"}, "./", "Path to destination directory")
+	Version      = flag.Bool([]string{"v", "-version"}, false, "Print the name and version")
 )
+
+func init() {
+	ManifestPath = flag.String([]string{"m", "-manifest"}, RequiredFlag("Manifest is required."), "Path to yaurtww manifest")
+}
+
+func RequiredFlag(ErrorMessage string) string {
+	log.Fatalln(ErrorMessage)
+	return "requiredstring"
+}
